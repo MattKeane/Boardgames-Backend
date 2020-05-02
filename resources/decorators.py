16bd37dcfe12af6deca36c05b62cde993a4cfbf2
Matt Keane <mattpkeane@gmail.com>
2020-05-02 16:02:22 -0500

    Improved publishers_only wrapper function

--- resources/decorators.py
+++ resources/decorators.py
@@ -1,13 +1,16 @@
 from flask_login import current_user
+from functools import wraps
 
 # requires logged in user to be a publisher
 
 def publishers_only(func):
-	if current_user.role == "publisher":
-		return func()
-	else:
-		return jsonify(
-			data = {},
-			message = "Action only available to registered publishers",
-			status = 401
-		), 401
+	@wraps(func)
+	def decorate_view(*args, **kwargs):
+		if current_user.role == "publisher":
+			return func(*args, **kwargs)
+		else:
+			return jsonify(
+				data = {},
+				message = "Action only available to registered publishers",
+				status = 401
+			), 401
