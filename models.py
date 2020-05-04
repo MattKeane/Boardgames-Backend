051c7b3422aacea7e6fdee67f48aa4421ef2abe4
Matt Keane <mattpkeane@gmail.com>
2020-05-04 16:06:27 -0500

    bug fix

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from playhouse.db_url import connect
 
 if "ON_HEROKU" in os.environ:
 	DATABASE = connect(os.environ.get("DATABASE_URL"))
-else
+else:
 	DATABASE = SqliteDatabase("games.sqlite",
 		pragmas = {"foreign_keys": 1})
 
